303a1d6bbb836f395fa1634d42b48feda2190dac
NatKwok <natkwok902@gmail.com>
2023-07-09 19:44:24 -0400

    Created Employee Model, Changed title

---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace asp_geosystems_store.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id {get; set;}
+        public string email {get; set;}
+        public string password {get; set;}
+        [DataType(DataType.Date)]
+        public DateTime dob {get; set;}
+        public int phone {get; set;}
+        public int phone {get; set;}
+        public string department {get; set;}
+
+    }
+}
