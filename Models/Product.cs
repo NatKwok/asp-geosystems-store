4c39cdb34f383ecbd2385c739c6142e757055453
NatKwok <natkwok902@gmail.com>
2023-07-19 20:38:00 -0400

    Created Product Model, Scaffolded Products

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace asp_geosystems_store.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+        
+
+    }
+}
