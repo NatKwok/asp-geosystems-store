d89ab28cefad48fd9903e2c75d8124883cf44e58
NatKwok <natkwok902@gmail.com>
2023-08-05 17:06:33 -0400

    Installed Tailwind, modified navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Pages/**/*.cshtml',
+    './Views/**/*.chstml'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
